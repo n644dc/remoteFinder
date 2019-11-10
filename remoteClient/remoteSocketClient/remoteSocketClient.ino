7fb18d7ea69670e651c2ccbbc1e49c2cabb2eb20
E R <n644dc@gmail.com>
2019-11-09 23:57:05 -0700

    updating websocket client code

--- remoteClient/remoteSocketClient/remoteSocketClient.ino
+++ remoteClient/remoteSocketClient/remoteSocketClient.ino
@@ -2,29 +2,73 @@
 #include "WebSocketClient.h"
 #include "ESP8266WiFi.h"
 
-WebSocketClient ws(true);
+WebSocketClient ws;
+WiFiClient client;
+
+char path[] = "/ws";
+char host[] = "52.90.5.163";
 
 void setup() {
 	Serial.begin(115200);
-	WiFi.begin("FreedomTemple", "clean&sober");
+  delay(500);
+
+  pinMode(0, OUTPUT);
+  digitalWrite(0, LOW);
+
+	WiFi.begin("FreedomTemple", "cleanTeam2800!");
 
 	Serial.print("Connecting");
 	while (WiFi.status() != WL_CONNECTED) {
 		delay(500);
 		Serial.print(".");
 	}
+  Serial.print("Connected to WIFI");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+  // Connect to the websocket server
+  if (client.connect("52.90.5.163", 8081)) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Connection failed.");
+    while(1) {
+      // Hang on failure
+    }
+  }
+
+  ws.path = path;
+  ws.host = host;
+  if (ws.handshake(client)) {
+    Serial.println("Handshake successful");
+  } else {
+    Serial.println("Handshake failed.");
+    while(1) {
+      // Hang on failure
+    }  
+  }
 }
 
 void loop() {
-	if (!ws.isConnected()) {
-		ws.connect("echo.websocket.org", "/", 443);
-	} else {
-		ws.send("hello");
+  String data;
+	if (!client.connected()) {
+    
+    Serial.println("Client disconnected.");
+    while (1) { 
+      // Hang on disconnect. }
+    }
 
-		String msg;
-		if (ws.getMessage(msg)) {
-			Serial.println(msg);
-		}
+	} else {
+    
+    ws.getData(data);
+    if (data.length() > 0) {
+      digitalWrite(0, HIGH);
+      delay(1000);
+      digitalWrite(0, LOW);
+      Serial.print("Received data: ");
+      Serial.println(data);
+    }
+
+    ws.sendData("hi");
 	}
-	delay(500);
+	delay(2000);
 }
