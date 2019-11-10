476b63273cbe8aaf3c6c1d19fd810df1ced4aaf4
E R <n644dc@gmail.com>
2019-11-10 02:28:10 -0700

    websocket client that appears to work

--- remoteClient/remoteSocketClient/remoteSocketClient.ino
+++ remoteClient/remoteSocketClient/remoteSocketClient.ino
@@ -1,74 +1,99 @@
+/*
+ * WebSocketClient.ino
+ *
+ *  Created on: 24.05.2015
+ *
+ */
+
 #include <Arduino.h>
-#include "WebSocketClient.h"
-#include "ESP8266WiFi.h"
 
-WebSocketClient ws;
-WiFiClient client;
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
 
-char path[] = "/ws";
-char host[] = "52.90.5.163";
+#include <WebSocketsClient.h>
+
+#include <Hash.h>
+
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+
+#define Serial Serial
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+  switch(type) {
+    case WStype_DISCONNECTED:
+      Serial.printf("[WSc] Disconnected!\n");
+      break;
+    case WStype_CONNECTED: {
+      Serial.printf("[WSc] Connected to url: %s\n", payload);
+
+      // send message to server when Connected
+      webSocket.sendTXT("Connected");
+    }
+      break;
+    case WStype_TEXT:
+      Serial.printf("[WSc] get text: %s\n", payload);
+
+      // send message to server
+      // webSocket.sendTXT("message here");
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      hexdump(payload, length);
+
+      // send data to server
+      // webSocket.sendBIN(payload, length);
+      break;
+        case WStype_PING:
+            // pong will be send automatically
+            Serial.printf("[WSc] get ping\n");
+            break;
+        case WStype_PONG:
+            // answer to a ping we send
+            Serial.printf("[WSc] get pong\n");
+            break;
+    }
+
+}
 
 void setup() {
-	Serial.begin(115200);
-  delay(500);
+  // Serial.begin(921600);
+  Serial.begin(115200);
 
-  pinMode(0, OUTPUT);
-  digitalWrite(0, LOW);
-
-	WiFi.begin("FreedomTemple", "cleanTeam2800!");
-
-	Serial.print("Connecting");
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-	}
-  Serial.print("Connected to WIFI");
-  Serial.println(WiFi.localIP());
+  Serial.println();
+  Serial.println();
   Serial.println();
 
-  // Connect to the websocket server
-  if (client.connect("52.90.5.163", 8081)) {
-    Serial.println("Connected");
-  } else {
-    Serial.println("Connection failed.");
-    while(1) {
-      // Hang on failure
-    }
+  for(uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
   }
 
-  ws.path = path;
-  ws.host = host;
-  if (ws.handshake(client)) {
-    Serial.println("Handshake successful");
-  } else {
-    Serial.println("Handshake failed.");
-    while(1) {
-      // Hang on failure
-    }  
+  WiFiMulti.addAP("FreedomTemple", "cleanTeam2800!");
+
+  //WiFi.disconnect();
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(100);
   }
+
+  // server address, port and URL
+  webSocket.begin("3.84.37.238", 80, "/ws");
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+  webSocket.sendTXT("Hi im here acter the onEnvet Call");
+  // try ever 5000 again if connection has failed
+  webSocket.setReconnectInterval(5000);
+  
+  // start heartbeat (optional)
+  // ping server every 15000 ms
+  // expect pong from server within 3000 ms
+  // consider connection disconnected if pong is not received 2 times
+  webSocket.enableHeartbeat(15000, 3000, 2);
+
 }
 
 void loop() {
-  String data;
-	if (!client.connected()) {
-    
-    Serial.println("Client disconnected.");
-    while (1) { 
-      // Hang on disconnect. }
-    }
-
-	} else {
-    
-    ws.getData(data);
-    if (data.length() > 0) {
-      digitalWrite(0, HIGH);
-      delay(1000);
-      digitalWrite(0, LOW);
-      Serial.print("Received data: ");
-      Serial.println(data);
-    }
-
-    ws.sendData("hi");
-	}
-	delay(2000);
+  webSocket.loop();
 }
