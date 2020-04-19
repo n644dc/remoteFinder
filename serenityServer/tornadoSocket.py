406c3f35ab6d2e96d681984661b3c8b0bc4ddca4
root <root@eriscore.localdomain>
2020-04-19 04:51:58 +0000

    Finished CRUD sqlite

--- serenityServer/tornadoSocket.py
+++ serenityServer/tornadoSocket.py
@@ -25,10 +25,11 @@ class SocketHandler(websocket.WebSocketHandler):
             self.dbc.execute('SELECT * FROM remotes')
         except Exception as ex:
             if 'no such table' in str(ex):
-                result = self.dbc.execute('''CREATE TABLE remotes (rid, rname, status)''')
-                self.dbc.commit()
+                self.dbc = self.dbRemotes.cursor()
+                result = self.dbc.execute('''CREATE TABLE remotes (rid, rname, status, time)''')
+                self.dbRemotes.commit()
 
-        print("DB is now in a valid op state.")
+        print("!!!! DB is Operational !!!!")
 
     def check_origin(self, origin):
         return True
@@ -42,6 +43,7 @@ class SocketHandler(websocket.WebSocketHandler):
     def on_close(self):
         if self in clientList:
             clientList.remove(self)
+        print("Client Disconnected")
 
     def on_message(self, message):
         print(message)
@@ -52,27 +54,55 @@ class SocketHandler(websocket.WebSocketHandler):
        data = {"type": "server", "value" : message}
        [con.write_message(data) for con in clientList]
 
+    def processMessage(self, message):
+        msgArray = [item.strip() for item in message.split(',')]
+        
+    def getRemote(self, rid):
+        if self.remoteExists(rid):
+            result = self.dbc.execute("SELECT * FROM remotes WHERE rid=?", (rid,))
+            res = self.dbc.fetchone()
+            returnRemote = {
+                "rid" : res[0],
+                "rname": res[1],
+                "status": res[2],
+                "time": res[3]
+            }
+            return returnRemote
+        return None
+
+    def getAllRemotes(self):
+        allList = []
+
+        result = self.dbc.execute("SELECT * FROM remotes")
+        for res in result:
+            returnRemote = {
+                "rid" : res[0],
+                "rname": res[1],
+                "status": res[2],
+                "time": res[3]
+            }
+            allList.append(returnRemote)
+        return allList
+
     def addRemote(self, remote):
         if not self.remoteExists(remote['rid']):
-            result = self.dbc.execute('INSERT INTO remotes VALUES ({0}, {1}, {2})'.format(remote['rid'], remote['name'], remote['state'], remote["time"]))
-            self.dbc.commit()
-        
-    def getRemote(self, remote):
-        if self.remoteExists(remote["rid"])
-            result = dbc.execute("SELECT * FROM remotes WHERE rid=?", (rid,))
-            reResult = dbc.fetchone()
-            print(reResult)
-            return reResult 
-        return None
-      
+            insertQry = "INSERT INTO remotes (rid, rname, status, time) VALUES ('{0}', '{1}', '{2}', '{3}')".format(remote['rid'], remote['rname'], remote['status'], remote["time"])
+            result = self.dbc.execute(insertQry)
+            self.dbRemotes.commit()
+        else:
+            self.updateRemote(remote)
+
     def updateRemote(self, remote):
-        dbc = self.dbRemotes.cursor()
-        result = self.dbc.execute('UPDATE remotes VALUES ({0}, {1}, {2})'.format(remote['rid'], remote['name'], remote['state']))
-        dbc.commit()
+        if self.remoteExists(remote['rid']):
+            updateQry = "UPDATE remotes SET rname = '{0}', status = '{1}', time = '{2}' WHERE rid = '{3}'".format(remote['rname'], remote['status'], remote["time"], remote['rid'])
+            result = self.dbc.execute(updateQry)
+            self.dbRemotes.commit()
+        else:
+            self.addRemote(remote)
       
     def remoteExists(self, rid):
         result = self.dbc.execute("SELECT * FROM remotes WHERE rid=?", (rid,))
-        if dbc.fetchone() is None:
+        if self.dbc.fetchone() is None:
             return False
         return True
 
@@ -83,15 +113,28 @@ def testSocketServer():
     sh = SocketHandler()
     #This is for testing
     testremote = {
-        "rid" : '1001',
+        "rid" : '1003',
         "rname": "room",
-        "status": "tooting"
+        "status": "tooting",
+        "time": "128348238429342384293423"
     }
 
     if sh.remoteExists(testremote["rid"]):
         print("it already exists")
     else:
-        print("we have to create it")
+        sh.addRemote(testremote)
+
+    print(sh.getRemote(testremote["rid"]))
+
+    testremote["status"] = "quiet"
+
+    sh.updateRemote(testremote)
+
+    print(sh.getRemote(testremote["rid"]))
+
+    rems = sh.getAllRemotes()
+    for rem in rems:
+        print(rem)
             
 ##############
 
